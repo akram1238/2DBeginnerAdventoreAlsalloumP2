f01e6f6f575692981172720b8581ae521d4ca5fc
akram1238 <754043@student.dusd.net>
2024-10-30 10:06:05 -0700

    write new sript for the character movement
    adding more object adding rigidbodys for the character and some objects.

--- 2DBeginnerAdventoreP2Alsalloum/Assets/Scrips/PlayControler.cs
+++ 2DBeginnerAdventoreP2Alsalloum/Assets/Scrips/PlayControler.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class PlayControler : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 50.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 50.0f * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
