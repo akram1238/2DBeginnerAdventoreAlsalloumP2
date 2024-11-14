70cff570a2c21de98b2f135a8eefd73e9cb5b113
akram1238 <754043@student.dusd.net>
2024-11-14 10:02:24 -0800

    finshed enemy zone
    I did finsh the enemy zone and make the enemy move up and down and left and right

--- 2DBeginnerAdventoreP2Alsalloum/Assets/Scrips/EnemyControler.cs
+++ 2DBeginnerAdventoreP2Alsalloum/Assets/Scrips/EnemyControler.cs
@@ -4,33 +4,51 @@ using UnityEngine;
 
 public class EnemyControler : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3.0f;
     public bool vertical;
+    public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2D;
 
+    float timer;
+    int direction = 1;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+
+        }
     }
-     void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed;
+            position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed;
+            position.x = position.x + Time.deltaTime * speed * direction;
         }
         rigidbody2D.MovePosition(position);
     }
-}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        PlayControler player = other.gameObject.GetComponent<PlayControler>();
+        if (player != null) { }
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}  
